e0a5f352461bacc7478c17d2fd0d681ebb4f3631
Ana <ana.pirvulescu@asaff.ro>
2013-06-21 14:27:13 +0300

    drag&drop working

--- Puzzle/gen/upb/puzzle/R.java
+++ Puzzle/gen/upb/puzzle/R.java
@@ -37,13 +37,14 @@ public final class R {
         public static final int stanga_sus=0x7f02000d;
     }
     public static final class id {
-        public static final int achievements=0x7f080002;
-        public static final int action_settings=0x7f080006;
+        public static final int achievements=0x7f080005;
+        public static final int action_settings=0x7f080007;
         public static final int editText1=0x7f080000;
-        public static final int imageView1=0x7f080001;
+        public static final int fereastra=0x7f080001;
         public static final int leaderboard=0x7f080003;
-        public static final int playFriends=0x7f080005;
-        public static final int randomPlay=0x7f080004;
+        public static final int playFriends=0x7f080006;
+        public static final int randomPlay=0x7f080002;
+        public static final int shuffleThis=0x7f080004;
     }
     public static final class layout {
         public static final int achievements=0x7f030000;
